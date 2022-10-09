dec8cd175619251721fef07effef69c57d46ad79
Khamala <elektrikamegawolt@gmail.com>
2022-10-09 22:19:21 +0200

    open-modal-franchise

---
+++ src/js/modal-for-franchise.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openFranchiseModalBtn: document.querySelector('[data-franchise-modal-open]'),
+      closeFranchiseModalBtn: document.querySelector('[data-franchise-modal-close]'),
+      modal: document.querySelector('[data-franchise-modal]'),
+    };
+  
+    refs.openFranchiseModalBtn.addEventListener('click', toggleModal);
+    refs.closeFranchiseModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('franchise-modal__is-hidden');
+      document.body.classList.toggle('no-scroll');
+    }
+})();
